ce42b492d65562bbe721bcc85cfdc36d737d61a6
Lukáš Lukča <xlukca@stuba.sk>
2024-01-28 15:18:54 +0100

    edit coffee migration and edit consumption blade

--- database/migrations/2024_01_13_224419_create_coffee_consumptions_table.php
+++ database/migrations/2024_01_13_224419_create_coffee_consumptions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('coffee_consumptions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('employee_id')->references('id')->on('employees');
+            $table->foreignId('employee_id')->constrained('employees')->onDelete('cascade');
             $table->string('year_of_order');
             $table->string('month_of_order');
             $table->string('day_of_order');
