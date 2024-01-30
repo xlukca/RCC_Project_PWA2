0e5152444bc74a3a00aa43cefe66e4fce5aff1f7
Matej Ružička <xruzickam@stuba.sk>
2024-01-30 16:50:59 +0100

    edit database

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -18,7 +18,7 @@ class EmployeeFactory extends Factory
     public function definition(): array
     {
 
-        $departmentIds = [4180, 4310, 4260, 4110, 4230, 4280, 4380, 4290, 4150, 4250, 4160, 4220, 4190, 4120, 4330, 4240, 4270, 4630, 4360, 4140, 4130, 4170, 4210, 4340, 4370];
+        $departmentIds = [4110, 4120, 4130, 4140, 4150, 4160, 4170, 4180, 4190, 4210];
 
         return [
             'first_name' => fake()->firstName(),
--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -24,21 +24,6 @@ class DepartmentSeeder extends Seeder
             ['id' => 4180,  'name_of_department' => 'Department of Analytical Chemistry', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
             ['id' => 4190,  'name_of_department' => 'Department of Inorganic Chemistry', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
             ['id' => 4210,  'name_of_department' => 'Department of Physical Chemistry', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4220,  'name_of_department' => 'Department of Information Engineering and Process Control', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4230,  'name_of_department' => 'Department of Chemical and Biochemical Engineering', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4240,  'name_of_department' => 'Department of Management', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4250,  'name_of_department' => 'Department of Food Science and Technology', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4260,  'name_of_department' => 'Department of Biochemistry and Microbiology', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4270,  'name_of_department' => 'Department of Mathematics', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4280,  'name_of_department' => 'Department of Chemical Physics', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4290,  'name_of_department' => 'Department of Enviromental Sciences', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4310,  'name_of_department' => 'Department of Biochemical Technology', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4330,  'name_of_department' => 'Department of Languages', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4340,  'name_of_department' => 'Department of Physical Education', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4360,  'name_of_department' => 'Department of Nutrition and Food Assessment', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4370,  'name_of_department' => 'Department of Plastics and Rubber', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4380,  'name_of_department' => 'Department of Chemical Technology of Wood Pulp and Paper', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
-            ['id' => 4630,  'name_of_department' => 'Department of NMR Spectroscopy and Mass Spectroscopy', 'street' => 'Radlinského 9', 'postcode' => '81237', 'city' => 'Bratislava', 'country' => 'Slovakia', 'created_at' => now()],
         ]);
     }
 }
