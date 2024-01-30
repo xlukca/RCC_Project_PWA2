0b3070121174d408d5d678809ac537e14d6d6b43
Matej Ružička <xruzickam@stuba.sk>
2024-01-30 01:27:18 +0100

    edit syntax notifications

--- resources/views/admin/notifications/email.blade.php
+++ resources/views/admin/notifications/email.blade.php
@@ -3,13 +3,12 @@
 </p>
 <p> 
 <br>    
-za mesiac  {{ $selectedMonths[0] }} evidujeme spotrebu kávy v počte  {{ $pocetK[$result->id] }}.
+za mesiac  {{ $selectedMonths[0] }} evidujeme spotrebu kávy v počte  {{ $coffee_num[$result->id] }}.
 <br>
-    Sumu {{ $pocetK[$result->id] * 0.3 }} € musíte uhradiť do {{ now()->add('+7 days')->format('Y-m-d') }}.
+    Sumu {{ $coffee_num[$result->id] * 0.3 }} € musíte uhradiť do {{ now()->add('+7 days')->format('Y-m-d') }}.
 <br> 
     Váš aktuálny zostatok na účte je {{ $result->payment->sum('income') - $result->consumption->count() * 0.3 }} €. 
 </p>
 <p>
     S pozdravom, Administrátor. 
 </p>
-
